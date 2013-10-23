704587f3c6ed5b72e1f427b9302c3cf877f72888
Rob Stenson <rob.stenson@gmail.com>
2013-10-23 10:39:41 -0400

    starting api integrations

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var width = 300,
 
 var canvas = d3.selectAll("canvas")
     .attr("width", width)
-    .attr("height", height)
+    .attr("height", height);
 
 var paths = Array.prototype.map.call(document.querySelectorAll("canvas"), function(c) {
   return d3.geo.path()
@@ -40,20 +40,69 @@ function ready(error, world, names) {
     return a.name.localeCompare(b.name);
   });
 
-  moveToRandomCountry();
+  startGame();
 }
 
-function moveToRandomCountry() {
-  paths.forEach(function(path, i) {
-    moveToCountry(path, countries[Math.floor(Math.random() * countries.length)], i);
+function format(fmt) {
+  var args = [].slice.call(arguments, 1);
+  return fmt.replace(/%@/g, function() {
+    var next = _.head(args);
+    args = _.tail(args);
+    return next ? next : "";
   });
 }
 
-function moveToCountry(path, country, i) {
-  d3.select(document.querySelectorAll("canvas")[i]).transition()
+function rand(limit) {
+  return Math.floor(Math.random() * limit);
+}
+
+function startGame() {
+  var randomCountries;
+  var correctChoice = 0;
+  var artist = null;
+
+  var nextRound = function() {
+    correctChoice = rand(paths.length);
+    randomCountries = _.map(paths, function() { return countries[rand(countries.length)]; });
+    _.each(randomCountries, moveToCountry);
+
+    var urlBase = "http://developer.echonest.com/api/v4/artist/search";
+    var url = format("%@?api_key=%@&format=json&artist_location=country:%@&bucket=artist_location&bucket=id:rdio-US&results=10",
+      urlBase, keys.echonest, randomCountries[correctChoice].name.replace(/\s/g, "%20"));
+
+    $.get(url, function(response) {
+      var artists = response.response.artists;
+      artist = artists[rand(artists.length)];
+      playArtist(artist);
+    });
+  };
+
+  d3.selectAll("div.choice-box")
+    .on("click", function(d, i) {
+      console.log(correctChoice);
+      _.each(paths, function(path, i) {
+        moveToCountry(randomCountries[correctChoice], i);
+      });
+
+      setTimeout(nextRound, 3000);
+    });
+
+  nextRound();
+}
+
+function playArtist(artist) {
+  console.log("playing ", artist);
+}
+
+function moveToCountry(country, i) {
+  var path = paths[i],
+      column = d3.selectAll(format("div.choice-box:nth-child(%@)", i+1)),
+      canvas = column.select("canvas");
+
+  canvas.transition()
       .duration(1250)
       .each("start", function() {
-        //title.text(country.name);
+        column.select("h3").text(country.name).style("color", colors[i]);
       })
       .tween("rotate", function() {
         var proj = path.projection(),
